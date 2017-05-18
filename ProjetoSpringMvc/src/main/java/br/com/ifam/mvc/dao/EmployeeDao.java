7136b23a0985c3c4c248bc7313e0f5cfce0bbe7a
wilson.neto <wilson.neto@DITIN14.tce.am.gov.br>
2017-05-18 13:29:05 -0400

    Configuração do projeto

---
+++ ProjetoSpringMvc/src/main/java/br/com/ifam/mvc/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package br.com.ifam.mvc.dao;
+
+import java.util.List;
+
+import br.com.ifam.mvc.model.Employee;
+
+public interface EmployeeDao {
+
+	Employee findById(int id);
+
+	void saveEmployee(Employee employee);
+	
+	void deleteEmployeeBySsn(String ssn);
+	
+	List<Employee> findAllEmployees();
+
+	Employee findEmployeeBySsn(String ssn);
+
+}
