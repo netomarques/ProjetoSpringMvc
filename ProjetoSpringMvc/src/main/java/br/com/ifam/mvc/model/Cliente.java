431eb762b7342833dfce5e8eb1468679b674797d
wilson.neto <wilson.neto@DITIN14.tce.am.gov.br>
2017-05-25 13:26:47 -0400

    Módulo cliente - inserido o atributo sexo e anotações de validação no modelo.

--- ProjetoSpringMvc/src/main/java/br/com/ifam/mvc/model/Cliente.java
+++ ProjetoSpringMvc/src/main/java/br/com/ifam/mvc/model/Cliente.java
@@ -5,11 +5,16 @@
  */
 package br.com.ifam.mvc.model;
 
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotBlank;
 
 /**
  *
@@ -18,21 +23,29 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name="cliente")
-public class Cliente {
+public class Cliente implements Serializable {
     
     @Id
     @GeneratedValue
     @Column(name = "cliid")
     private int id;
     
+    @NotBlank(message= "Nome é obrigatório")
     @Column(name = "clinome", nullable = false, length = 80)
     private String nome;
     
+    @NotBlank(message= "Login é obrigatório")
     @Column(name = "clilogin", nullable = false,length = 60)
     private String login;
     
-    @Column(name = "clisenha", nullable = false, length =60)
+    @NotBlank(message= "Senha é obrigatória")
+    @Column(name = "clisenha", nullable = false, length = 60)
     private String senha;
+    
+    @NotNull(message= "Informe o sexo")
+    @Enumerated(EnumType.STRING)
+    @Column(name = "clisexo", nullable=false, length = 15)
+    private Sexo sexo;
 
     public int getId() {
         return id;
@@ -64,6 +77,14 @@ public class Cliente {
 
     public void setSenha(String senha) {
         this.senha = senha;
+    }
+    
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
     }
     
 }
